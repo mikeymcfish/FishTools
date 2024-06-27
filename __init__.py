e25a0b44a0d3af8c2cd42778270c7646fd43442c
fish <mikefischthal@gmail.com>
2024-06-27 08:32:55 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,7 +5,6 @@
 @description: This extension provides tools for generating laser cutter ready files and other fun stuff
 """
 from .LaserCutterFull import LaserCutterFull
-from .Deptherize import Deptherize
 from .AnaglyphCreator import AnaglyphCreator
 
 # I copied this init from LtCmData
@@ -14,17 +13,14 @@ print(f"### Loading: FishTools ({version_str})")
 
 node_list = [
     "LaserCutterFull",
-    "Deptherize",
     "AnaglyphCreator"
 ]
 
 NODE_CLASS_MAPPINGS = {
     "LaserCutterFull": LaserCutterFull,
-    "Deptherize" : Deptherize,
     "AnaglyphCreator" : AnaglyphCreator
     }
 NODE_DISPLAY_NAME_MAPPINGS = {
     "LaserCutterFull": "LaserCutterFull",
-    "Deptherize" : "Deptherize",
     "AnaglyphCreator": "AnaglyphCreator"
     }
